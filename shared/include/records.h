95fc9b733272e7532e1789ca630a13291468cb73
Sergey Popov <sergey.snip@gmail.com>
2012-05-18 21:50:13 +0400

    records implemented

---
+++ shared/include/records.h
@@ -0,0 +1,30 @@
+/*
+ * records.h
+ *
+ *  Created on: May 11, 2012
+ *      Author: Sergey Popov (snip89@mail.ru)
+ */
+
+#ifndef RECORDS_H_
+#define RECORDS_H_
+
+#include <QByteArray>
+#include <QList>
+#include <QVariant>
+#include <QString>
+
+struct Record
+{
+    QByteArray byteRecord;
+    quint64 vTime;
+    quint8 eventID;
+    QList<QVariant> other;
+};
+
+struct RecordArgument
+{
+    QString *name;
+    QVariant *value;
+};
+
+#endif /* RECORDS_H_ */
