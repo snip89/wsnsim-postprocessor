99a96f6968bd725f70162b1753a291ab80871c15
Sergey Popov <sergey.snip@gmail.com>
2012-05-18 21:49:39 +0400

    logdatatypes implemented

---
+++ shared/include/logdatatypes.h
@@ -0,0 +1,25 @@
+/*
+ * logdatatypes.h
+ *
+ *  Created on: May 7, 2012
+ *      Author: Sergey Popov (snip89@mail.ru)
+ */
+
+#ifndef LOGDATATYPES_H_
+#define LOGDATATYPES_H_
+
+enum LogDataType
+{
+    UNKNOWN_TYPE,
+    UINT8_TYPE,
+    UINT16_TYPE,
+    UINT32_TYPE,
+    UINT64_TYPE,
+    INT32_TYPE,
+    BOOL_TYPE,
+    DOUBLE_TYPE,
+    BYTE_ARRAY_TYPE,
+    STRING_TYPE
+};
+
+#endif /* LOGDATATYPES_H_ */
