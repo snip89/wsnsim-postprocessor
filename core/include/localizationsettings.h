c1ff053e2d5da8202a8579d0ce5c3319a06df1fd
Sergey Popov <sergey.snip@gmail.com>
2012-08-23 20:39:57 +0400

    localizationsettings reimplemented

---
+++ core/include/localizationsettings.h
@@ -0,0 +1,43 @@
+/*
+ *    Created by Sergey Popov
+ *    snip89@mail.ru
+ */
+
+#ifndef LOCALIZATIONSETTINGS_H
+#define LOCALIZATIONSETTINGS_H
+
+#include <QWidget>
+#include <QString>
+#include <QSettings>
+#include <QAbstractButton>
+#include <QMessageBox>
+
+#include "ui_localizationsettings.h"
+
+namespace Ui {
+    class LocalizationSettings;
+}
+
+class LocalizationSettings : public QWidget
+{
+    Q_OBJECT
+
+public:
+    LocalizationSettings(QWidget *parent = 0);
+
+    void setSettingsName(QString name);
+    void showCurrentSettings();
+    void applySettings();
+
+    ~LocalizationSettings();
+private:
+    Ui::LocalizationSettings *ui;
+
+    QSettings settings;
+
+    void showDefaultSettings();
+private slots:
+    void buttonClicked(QAbstractButton *button);
+};
+
+#endif // LOCALIZATIONSETTINGS_H
